60de344f56e76208620c1b7ed057fcb86adcbc92
angmavrogiannis <angmavrogiannis@gmail.com>
2024-03-01 03:12:21 -0500

    Added current version

--- lateral_control.py
+++ lateral_control.py
@@ -8,7 +8,6 @@ import robomaster
 from robomaster import robot
 import numpy as np
 import cv2
-import os
 
 def normalize_coords(pixel_coord, dimension):
     return 2 * pixel_coord / dimension - 1
@@ -45,34 +44,39 @@ if __name__ == '__main__':
     ep_robot.initialize(conn_type="sta")
     ep_chassis = ep_robot.chassis
     ep_camera = ep_robot.camera
-
-    # Initializing and subscribing to distance sensor
-    ep_sensor = ep_robot.sensor
-    ir_distance = Distance()
-    ep_sensor.sub_distance(freq=5, callback=ir_distance.sub_data_handler)
-    time.sleep(0.25)
+    save_to_file = False
 
     # Get the latest 1 frame image display each time, and stay for 1 second
     ep_camera.start_video_stream(display=False)
     curr_frame = ep_camera.read_cv2_image(strategy="newest")
-    cv2.imwrite("ovd.jpg", curr_frame)
+    height, width, channels = curr_frame.shape
+    draw_bb = False
+    if draw_bb:
+        (xmin, ymin, boxw, boxh) = cv2.selectROI("Select Object", curr_frame)
+        cv2.destroyWindow("Select Object")
+        curr_bbox = np.array([[[xmin,ymin],[xmin+boxw,ymin],[xmin,ymin+boxh],[xmin+boxw,ymin+boxh]]]).astype(float)
+    else:
+        cv2.imwrite("curr_frame.jpg", curr_frame)
 
-    # Run GLIP to detect object on the cluster
-    exec(open("client.py").read())
-    with open("bbox.txt", "r") as f:
-        detected_bbox_coords = f.read().rstrip()
-    os.system("rm -rf bbox.txt")
+        # Run GLIP to detect object on the cluster
+        # exec(open("client.py").read())
+        with open("bbox.txt", "r") as f:
+            detected_bbox_coords = f.read().rstrip()
 
-    # [xmin, ymin, xmax, ymax]
-    detected_bbox_coords = [int(val) for val in detected_bbox_coords.strip('][').split(', ')]
-    xmin = detected_bbox_coords[0]
-    ymin = detected_bbox_coords[1]
-    xmax = detected_bbox_coords[2]
-    ymax = detected_bbox_coords[3]
-    boxw = xmax - xmin
-    boxh = ymax - ymin
+        # [xmin, ymin, xmax, ymax]
+        detected_bbox_coords = [int(val) for val in detected_bbox_coords.strip('][').split(', ')]
+        xmin = detected_bbox_coords[0]
+        ymin = detected_bbox_coords[1]
+        xmax = detected_bbox_coords[2]
+        ymax = detected_bbox_coords[3]
+        boxw = xmax - xmin
+        boxh = ymax - ymin
 
-    curr_bbox = np.array([[[xmin, ymin],[xmax, ymin],[xmin, ymax],[xmax, ymax]]]).astype(float)
+        curr_bbox = np.array([[[xmin, ymin],[xmax, ymin],[xmin, ymax],[xmax, ymax]]]).astype(float)
+
+    if save_to_file:
+        out = cv2.VideoWriter('output.avi',0,cv2.VideoWriter_fourcc(*'MJPG'),10,(width, height))
+
     # curr_bbox = np.array([[[291,187],[405,187],[291,267],[405,267]]]).astype(float)
     # Get features from the initial frame for KLT Tracking
     startXs,startYs = getFeatures(cv2.cvtColor(curr_frame, cv2.COLOR_RGB2GRAY), curr_bbox, use_shi=False)
@@ -80,22 +84,13 @@ if __name__ == '__main__':
     finished = False
     curr_time = 0
     prev_time = 0
-    lateral_controller = PID(2, 0, 0.1, 0.1)
-    longitudinal_controller = PID(0.4, 0, 0.1, 0.1)
-    lateral_threshold = 10
-    longitudinal_threshold = 10
-
-    height, width, channels = curr_frame.shape
-    max_distance = 6000
+    lateral_controller = PID(0.1, 0.01, 0.2, 0.1) #kp was 2
+    lateral_threshold = 20
 
     # target lateral position: centering object patch in image frame
     target_x_pos = int(width / 2)
 
-    # target longitudinal position: 200mm from the object
-    target_z_pos = 260
-
     play_realtime = True
-    save_to_file = False
     print("Entering loop")
     try:
         while finished == False:
@@ -116,41 +111,27 @@ if __name__ == '__main__':
 
             # update feature points as required
             n_features_left = np.sum(Xs != -1)
-            print('# of Features: %d' % n_features_left)
+            # print('# of Features: %d' % n_features_left)
             if n_features_left < 15:
-                print('Generate New Features')
+                # print('Generate New Features')
                 startXs,startYs = getFeatures(cv2.cvtColor(curr_frame, cv2.COLOR_RGB2GRAY), curr_bbox)
 
             # Compute the lateral coordinate of the center of the patch
-            cX = int(xmin + boxw / 2)
-
-            # Get the reading for the current distance to the object from the IR sensor
-            cZ = ir_distance.distance # param: port_id
+            cX = int(np.mean(startXs[startXs != -1.]))
 
             print("Center of patch x position in image frame: ", cX)
-            print("Current distance from object: ", cZ)
-            # print("Image center x position: {}".format(target_y_pos))
 
             lateral_error = cX - target_x_pos
             norm_lateral_error = normalize_coords(cX, width) - normalize_coords(target_x_pos, width)
 
-            longitudinal_error = cZ - target_z_pos
-            norm_longitudinal_error = normalize_coords(cZ, max_distance) - normalize_coords(target_z_pos, max_distance)
-
             print(f"Lateral error in pixels: {lateral_error}")
-            print(f"Longitudinal error in pixels: {longitudinal_error}")
-            if abs(lateral_error) < lateral_threshold and abs(longitudinal_error) < longitudinal_threshold:
+            if abs(lateral_error) < lateral_threshold:
                 finished = True
-                print("Done, ready to pick up the object!")
+                print("Done, camera centered onto object!")
             else:
-                if abs(lateral_error) >= lateral_threshold:
-                    pid_output_x = lateral_controller.update(norm_lateral_error, dt)
-                    print(f"PID X Output: {pid_output_x}")
-                    ep_chassis.drive_speed(x=0, y=pid_output_x, z=0, timeout=5)
-                elif abs(longitudinal_error) >= longitudinal_threshold:
-                    pid_output_z = longitudinal_controller.update(norm_longitudinal_error, dt)
-                    print(f"PID Z Output: {pid_output_z}")
-                    ep_chassis.drive_speed(x=pid_output_z, y=0, z=0, timeout=5)
+                pid_output_x = lateral_controller.update(norm_lateral_error, dt)
+                print(f"PID X Output: {pid_output_x}\n")
+                ep_chassis.drive_speed(x=0, y=pid_output_x, z=0, timeout=1)
 
             prev_time = curr_time
 
@@ -158,8 +139,11 @@ if __name__ == '__main__':
             frame_draw = curr_frame.copy()
             (xmin, ymin, boxw, boxh) = cv2.boundingRect(curr_bbox[:, :].astype(int))
             frame_draw = cv2.rectangle(frame_draw, (xmin, ymin), (xmin + boxw, ymin + boxh), (255,0,0), 2)
+
+
             for k in range(startXs.shape[0]):
                 frame_draw = cv2.circle(frame_draw, (int(startXs[k]), int(startYs[k])), 3, (0, 0, 255), thickness=2)
+            frame_draw = cv2.circle(frame_draw, (cX, int(np.mean(startYs[startYs != -1.]))), 3, (0, 255, 0), thickness=6)
             
             # imshow if to play the result in real time
             if play_realtime:
@@ -169,6 +153,9 @@ if __name__ == '__main__':
 
             cv2.waitKey(1)
             time.sleep(1)
+
+        if save_to_file:
+            out.release()
 
     except KeyboardInterrupt:
         ep_chassis.drive_speed(x=0, y=0, z=0, timeout=5)
